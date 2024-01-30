50d2a7b872fb8ecaa847eb5cf4be9c921e83c334
Dan Baird <13587231+thedanbaird@users.noreply.github.com>
2024-01-30 11:22:26 +1100

    Add tag manager

--- src/header.php
+++ src/header.php
@@ -30,11 +30,25 @@
     <meta property="og:url" content="<?php echo the_permalink(); ?>"/>
 	<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/og.jpg"/>
 
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-MXWK4KLV');</script>
+	<!-- End Google Tag Manager -->
+
 	<?php wp_head(); ?>
 
 </head>
 
 <body <?php body_class(); ?>>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MXWK4KLV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <?php wp_body_open(); ?>
 
 <?php get_template_part( 'template-parts/header', 'layout' ); ?>
